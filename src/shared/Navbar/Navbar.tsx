b6288f5dbf8f44f38cdc67d8d190e366a2e76748
Naofel Badsha <naofelbadsha@gmail.com>
2026-03-21 23:13:41 +0600

    adddd theme

--- src/shared/Navbar/Navbar.tsx
+++ src/shared/Navbar/Navbar.tsx
@@ -5,11 +5,22 @@ import Link from "next/link"
 const Navbar = () => {
     return (
         <header className="py-5 border-b border-b-border">
-            <Container>
+            <Container className="flex items-center justify-between gap-5">
+                {/*-------Logo------*/}
                 <div className="">
                     <Link href={"/"}>
-                        <h2 className="">NexCart</h2>
+                        <h2 className="text-md font-semibold font-poppins text-shop_dark_green">NexCart</h2>
                     </Link>
+                </div>
+
+                {/*---------Navbar--------*/}
+                <div className="">
+                    Navigation
+                </div>
+
+                {/*---------Icon--------*/}
+                <div className="">
+                    Icon
                 </div>
             </Container>
         </header>
