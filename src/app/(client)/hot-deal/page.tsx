3b8617322e1010bd65ac464bf1284bcb6f2ea355
Naofel Badsha <naofelbadsha@gmail.com>
2026-03-22 20:35:49 +0600

    added nave item

---
+++ src/app/(client)/hot-deal/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HotDealPage = () => {
+  return (
+    <div>HotDealPage</div>
+  )
+}
+
+export default HotDealPage
--- src/shared/Navbar/Navbar.tsx
+++ src/shared/Navbar/Navbar.tsx
@@ -1,11 +1,14 @@
+'use client'
 import Container from "@/components/common/Container"
 import { ModeToggle } from "@/components/ui/mode-toggle"
 import headerNavItems from "@/constants/data"
 import { cn } from "@/lib/utils"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 
 const Navbar = () => {
+    const pathname = usePathname()
     return (
         <header className="py-5 border-b border-b-border">
             <Container className="flex items-center justify-between gap-5">
@@ -17,10 +20,14 @@ const Navbar = () => {
                 </div>
 
                 {/*---------Navbar--------*/}
-                <div className="hidden md:inline-flex items-center justify-center gap-7 text-sm font-semibold text-light-color capitalize">
+                <div className="hidden md:inline-flex items-center justify-center gap-7 text-sm font-semibold text-light-color capitalize relative">
                     {
                         headerNavItems?.map((item) => (
-                            <Link key={item.id} className={cn("hover:text-shop_light_green hoverEffect")} href={item.href}>{item.title}</Link>
+                            <Link 
+                            key={item.id} 
+                            className={cn("hover:text-shop_light_green hoverEffect ", pathname === item?.href && "text-light-green")} href={item?.href}
+                            >{item?.title}
+                            </Link>
                         ))
                     }
                 </div>
