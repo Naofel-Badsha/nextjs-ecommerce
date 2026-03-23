1af9c515ee5865c0b3bd208b3f611f40f398d0a5
Naofel-Badsha <naofelbadsha@gmail.com>
2026-03-23 21:26:35 +0600

    added footer top type defination

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { FaClock, FaMailBulk, FaMap, FaPhone } from "react-icons/fa";
 
 const Footer = () => {
+
+  interface ContactItem {
+  icon: React.ReactNode;   
+  title: string;        
+  content: string;   
+}
+  const contactDetails: ContactItem[] = [
+    {
+      icon: <FaMap className="w-6 h-6 text-slate-600" />,
+      title: "Visit Us",
+      content: "123 Shopping Street, Commerce District, New York, NY 10001, USA",
+    },
+    {
+      icon: <FaPhone className="w-6 h-6 text-slate-600" />,
+      title: "Call Us",
+      content: "+1 (555) 123-4567",
+    },
+    {
+      icon: <FaClock className="w-6 h-6 text-slate-600" />,
+      title: "Working Hours",
+      content: "Monday - Friday: 9AM - 6PM",
+    },
+    {
+      icon: <FaMailBulk className="w-6 h-6 text-slate-600" />,
+      title: "Email Us",
+      content: "support@shopcart.com",
+    },
+  ];
   return (
-    <div>Footer</div>
+    <footer>
+      {/*--------Contact----Info---------*/}
+    </footer>
   )
 }
 
