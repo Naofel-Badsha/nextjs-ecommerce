6e795e07f436d2a1948ecd2e0f126c7659b63979
Naofel Badsha <naofelbadsha@gmail.com>
2026-03-26 02:37:02 +0600

    added auth content

--- src/app/(auth)/sing-up/[[...sing-up]]/page.tsx
+++ src/app/(auth)/sing-up/[[...sing-up]]/page.tsx
@@ -1,8 +1,38 @@
-"use client"
+"use client";
+
+import Container from "@/components/common/Container";
+import { contactConfig } from "@/config/contactConfig";
+import { motion } from "motion/react";
+
 const SingUpPage = () => {
   return (
-    <div className="min-h-screen bg-linear-to-br from-shop_orange/5 via-shop_light_bg to-shop_light_green/45 relative overflow-hidden">SingUpPage</div>
-  )
-}
+    <div className="min-h-screen bg-linear-to-br from-shop_orange/5 via-shop_light_bg to-shop_light_green/45 relative overflow-hidden">
+      <Container className="flex flex-col lg:flex-row gap-5 lg:gap-10">
+        {/*-------Left------Side------*/}
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.2 }}
+          className="w-full lg:w-3/5"
+        >
+          <div className="">
+            <motion.div className="">
+              <h1 className="text-4xl sm:text-3xl font-medium text-shop_dark_green mb-5 font-poppins">
+                Jion {contactConfig.company.name}
+              </h1>
+              <p className="max-w-lg font-poppins">
+                Create your account and unlock exclusive benefits, personalized
+                recommendations and seamless shopping exeperiences
+              </p>
+            </motion.div>
+          </div>
+        </motion.div>
 
-export default SingUpPage
+        {/*-------Right------Side------*/}
+        <motion.div className="w-full lg:w-2/5">right</motion.div>
+      </Container>
+    </div>
+  );
+};
+
+export default SingUpPage;
--- src/shared/Navbar/AuthHeader.tsx
+++ src/shared/Navbar/AuthHeader.tsx
@@ -5,10 +5,9 @@ import { FaArrowLeft } from 'react-icons/fa'
 
 const AuthHeader = () => {
   return (
-    <div className='bg-linear-to-br from-shop_orange/5 via-shop_light_bg to-shop_light_pink/60'>
-      AuthHeader
+    <div className='bg-linear-to-br from-shop_orange/5 via-shop_light_bg to-shop_light_pink/60 py-5 shadow'>
       <Container className='flex items-center justify-between'>
-        <Link href="/" className='flex items-center gap-1'><FaArrowLeft /><span >Back to Home</span></Link>
+        <Link href="/" className='flex items-center gap-1 hover:text-shop_light_green'><FaArrowLeft /><span className='font-medium'>Back to Home</span></Link>
         <div className="">
           <Link href={"/"}>
             <h2 className="text-md font-semibold font-poppins text-shop_dark_green">NexCart</h2>
