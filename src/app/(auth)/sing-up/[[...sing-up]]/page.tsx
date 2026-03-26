2d51e68a29ac04f78bfbcb96f44e7d07c352d29c
Naofel Badsha <naofelbadsha@gmail.com>
2026-03-26 23:03:21 +0600

    added auth icon

--- src/app/(auth)/sing-up/[[...sing-up]]/page.tsx
+++ src/app/(auth)/sing-up/[[...sing-up]]/page.tsx
@@ -2,6 +2,7 @@
 
 import Container from "@/components/common/Container";
 import { contactConfig } from "@/config/contactConfig";
+import { singUpBenefits } from "@/constants/data";
 import { motion } from "motion/react";
 
 const SingUpPage = () => {
@@ -10,13 +11,18 @@ const SingUpPage = () => {
       <Container className="flex flex-col lg:flex-row gap-5 lg:gap-10">
         {/*-------Left------Side------*/}
         <motion.div
-          initial={{ opacity: 0, y: 20 }}
+          initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, delay: 0.2 }}
-          className="w-full lg:w-3/5"
+          className="w-full lg:w-3/5 py-8 lg:py-10"
         >
           <div className="">
-            <motion.div className="">
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, delay: 0.2 }}
+              className=""
+            >
               <h1 className="text-4xl sm:text-3xl font-medium text-shop_dark_green mb-5 font-poppins">
                 Jion {contactConfig.company.name}
               </h1>
@@ -25,6 +31,18 @@ const SingUpPage = () => {
                 recommendations and seamless shopping exeperiences
               </p>
             </motion.div>
+            {/*---------Benefites--------*/}
+            <div className="space-y-4">
+              <div className="space-y-6">
+                {singUpBenefits?.map((benefit, index) => (
+                  <motion.div key={index} className="flex items-center gap-4 mt-5">
+                    <div className="p-2 bg-shop_orange/10 rounded-lg shrink-0">
+                      <benefit.icon />
+                    </div>
+                  </motion.div>
+                ))}
+              </div>
+            </div>
           </div>
         </motion.div>
 
